ff66cf02522110a1e5a2abe4ea0121c24a628ec7
黄添榕 <1757968963@qq.com>
2020-11-08 22:34:39 +0800

    feat(component): 增加了登录页面和注册页面
    增加了登陆页面和注册页面，由何悦完成登录页面，梓仪完成登陆页面，我接着完成index主题结构的搭建
    
    #3

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/components/Index'
+import Index from '@/pages/Index'
+import Login from '@/pages/Login'
+import Register from '@/pages/Register'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'blog',
-      component: Index
-    }
+    { path: '/',name: 'blog',component:Index },
+    { path:'/login', name:'login',component:Login },
+    { path:'/register', name:'register',component:Register }
   ]
 })
