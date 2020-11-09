2d2cd87a794d89dc20fbf396aa7c9e5b84776e6b
黄添榕 <1757968963@qq.com>
2020-11-09 23:16:53 +0800

    feat(index.vue): 搭建了基础的页面结构
    #3

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,13 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,50 @@
 <template>
-  <div>hello world</div>
+  <div style="width: 100%; height: 100%">
+    <!-- 页面容器 -->
+    <el-container id="bigContainer">
+      <el-header>纯享版博客园</el-header>
+      <el-container id="underContent">
+        <el-aside style="width: 200px; height: 100%; background-color: #545c64">
+          <side-menu></side-menu>
+        </el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
-export default {};
+import SideMenu from "../components/SideMenu";
+export default {
+  data() {
+    return {
+      isCollapse: true,
+    };
+  },
+  methods: {},
+  components: { SideMenu },
+};
 </script>
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+#bigContainer {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+#underContent {
+  flex-grow: 1;
+}
 </style>
