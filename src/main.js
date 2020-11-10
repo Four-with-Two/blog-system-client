118b4b08a8aa71736902d2d8255ebe364efe0b2d
黄添榕 <1757968963@qq.com>
2020-11-10 14:18:34 +0800

    feat(index.vue): 完成了index路由的切换配置
    #4

--- src/main.js
+++ src/main.js
@@ -3,20 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {RadioGroup,RadioButton,Menu,Submenu,MenuItemGroup,MenuItem,Container,Header,Aside,Footer,Main} from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'; //还需要引入样式表才能使用
-
-Vue.use(Container)//使用use是用来全局注册插件
-Vue.use(Header)
-Vue.use(Footer)
-Vue.use(Aside)
-Vue.use(Main)
-Vue.use(RadioGroup)
-Vue.use(RadioButton)
-Vue.use(Menu)
-Vue.use(Submenu)
-Vue.use(MenuItemGroup)
-Vue.use(MenuItem)
+import './plugins/element'
 
 Vue.config.productionTip = false
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,8 +8,16 @@
           <side-menu></side-menu>
         </el-aside>
         <el-container>
-          <el-main>Main</el-main>
-          <el-footer>Footer</el-footer>
+          <el-main>
+            <el-breadcrumb separator-class="el-icon-arrow-right">
+              <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+              <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+              <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+              <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+            </el-breadcrumb>
+            <router-view></router-view>
+          </el-main>
+          <el-footer>参与成员：四带二队</el-footer>
         </el-container>
       </el-container>
     </el-container>
