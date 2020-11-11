f24fa8bea4bb9f59dacdfac3b0fdb039f8a2c607
yue <123456@qq.com>
2020-11-11 15:40:39 +0800

    feat: just for test

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="register">
-    <div class="box">
-      <h1 id="header">纯享博客注册页面</h1>
-      <div class="user_name">
-        <input type="text" required />
-        <label>
-          <span>Username</span>
-        </label>
+    <div class="head">
+      <div class="left">
+        <a href="http://localhost:8080/#/index/blogGround">&nbsp&nbsp<<博客广场</a>
       </div>
+      <div class="right">
+        [ <a href="http://localhost:8080/#/login">登录</a> . <a href="http://localhost:8080/#/register">注册</a> ]&nbsp&nbsp
+      </div>
+    </div>
+    <div class="box">
+      <h1 id="title">纯享博客注册页面</h1>
+      <div class="user_name">
+        <input type="text" required placeholder="请输入中文用户名"/>
+      </div>
+      <div class="email">
+        <div class="ema_in">
+          <input type="text" required placeholder="请输入邮箱地址"/>
+        </div>
+        
+      </div>
+      <div class="password">
+        <input type="text" required placeholder="请输入密码"/>
+      </div>
+      <div class="btn">
+        <a href="">立刻注册</a>
+        </div>
     </div>
   </div>
 
@@ -18,64 +35,83 @@ export default {};
 </script>
 
 <style scoped>
+
+.head{
+  height: 40px;
+  width: 100%;
+  background-color: #c0c0c0;
+  border-bottom: 1px solid #484848;
+
+}
+.head a{
+  text-decoration: none;
+  color: #0066ff;
+}
+
 .register{
        height:100%;
        width: 100%;
-       background-color: #9966FF;
        float:left;
+}
+.left{
+  float: left;
+  margin-top: 8px;
+}
+.right{
+  float: right;
+  margin-top: 8px;
 }
 .box{
    background-color: azure;
    width: 30%;
    height: 40%;
-   top: 25%;
+   top: 20%;
    margin: auto;
    position: relative;
    border: 1px solid;
    border-radius: 10px;
 }
-#header{
+#title{
   text-align: center;
   margin-top: 10px ;
 }
-.user_name{
-    width: 50%;
-    height: 50vh;
-    margin: auto;
-    position: relative;
-    font-family: Sans-serif;
+.box input{
+    outline-style: none ;
+    border: 1px solid #ccc; 
+    border-radius: 3px;
+    padding: 13px 14px;
+    width: 80%;
+    font-size: 18px;
+    font-weight: 300;
+    margin-left: 7.5%;
+    margin-top: 5%;
 }
-.user_name input{
-  height: 2rem;
-  width: 10rem;
-  border: none;
-  border-bottom: 0.1rem solid black;
-  padding-top:1rem ;
-  outline: none;
+.box input:focus{
+    border-color: #66afe9;
+    outline: 0;
+    -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6);
+    box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6)
+}
+.btn{
 
+  margin: auto;
+  margin-top: 4%;
+  text-align: center;
 }
-.user_name label{
-  position: absolute;
-  top: 0;
-  left: 0;
-  pointer-events: none;
-}
-.user_name label span{
-   position: absolute;
-  bottom: -3rem;
-  left: 0;
-  transition: all 0.3s ease; 
-}
-.user_name ::after{
-  content:"";
-  height: 3rem;
-  width: 10rem;
-  top: 0;
-  left: 0;
-  border-bottom: 0.2rem solid #1cb9b6;
-  transform: translateX(-100%);
-  transition:all 0.3s ease;
-}
+.btn a{
+    width: 70%;
+    color: #fff;
+    background: #3b78dd;
+    font-size: 22px;
+    letter-spacing: 2.2px;
+    line-height: 48px;
+    display: block;
+    border-radius: 3px;
+    margin: auto;
+    text-decoration: none
+  
+};
+
 
     
 </style>
